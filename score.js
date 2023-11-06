44d25b10dac460d4ead4728cac5c3bd848c755d4
Piyush Bachani <Piyush>
2023-11-06 11:14:46 +0530

    Updated the Game

--- score.js
+++ score.js
@@ -1,1 +1,14 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+let score = localStorage.getItem("score");
+console.log(score);
+var scoreBoard = document.getElementById("score-board")
+
+scoreBoard.innerHTML = score;
+
+
+const play_again_button = document.getElementById("play-again-button")
+function playagain(){
+    window.location.href="./game.html"
+}
+play_again_button.addEventListener("click", playagain);
